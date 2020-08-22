7521cad543f92073141380857043a64f2a0e9af9
dev-joe95 <joe.afify95@gmail.com>
2020-08-22 16:29:15 +0200

    change routes names

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const router = require('./routes/genre.routes')
+const genreRouter = require('./routes/genre.routes')
+const customerRouter = require('./routes/customer.routes')
 const express = require('express');
 const app = express();
 
@@ -7,15 +8,18 @@ mongoose.connect('mongodb://localhost:27017/vidly', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('connected to mongodb'))
-    .catch(err => console.error("can't connect to mongodb", err))
+    .then(() => console.log('Connected to mongodb'))
+    .catch(err => console.error("Can't connect to mongodb", err))
+
+mongoose.set('useFindAndModify', false);
 
 app.use(express.json());
-app.use('/api/genres', router)
+app.use('/api/genres', genreRouter)
+app.use('/api/customers', customerRouter)
 
 
 app.use(express.static('/public'));
 const port = process.env.PORT || `3030`;
 app.listen(3030, () => {
-    console.log(`Your app is connected on http://localhost:${port}`);
+    console.log(`Your app is running on http://localhost:${port}`);
 })
