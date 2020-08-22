486d416d1b5455cf0bb89d9c6331473a07b8bd43
dev-joe95 <joe.afify95@gmail.com>
2020-08-22 16:27:39 +0200

    small edit on genre api

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -3,33 +3,36 @@ const Genre = require('../models/genre');
 
 exports.listAllGenres = async (req, res) => {
     await Genre.find({}, (err, genre) => {
-        if (err) res.send(err);
+        if (err) res.status(400).send(err);
         res.json(genre);
-    })
+    }).sort('name')
 };
 
 exports.listGenreById = async (req, res) => {
-    // return genres.find((g) => g.id === req.params.id);
+    await Genre.findById(req.params.id, (err, genre) => {
+        if (err) res.status(400).send(err);
+        res.json(genre);
+    })
 };
 
 exports.AddGenre = async (req, res) => {
     const newGenre = new Genre(req.body);
     await newGenre.save((err, genre) => {
-        if (err => res.send(err))
+        if (err => res.status(400).send(err))
             res.json(genre);
     });
 };
 
 exports.UpdateGenre = async (req, res) => {
     await Genre.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, genre) => {
-        if (err) res.send(err);
+        if (err) res.status(400).send(err);
         res.json(genre);
     })
 };
 
 exports.deleteGenre = async (req, res) => {
     await Genre.findByIdAndRemove(req.params.id, (err, genre) => {
-        if (err) res.send(err);
+        if (err) res.status(404).send(err);
         res.json({ message: "Successfully deleted" });
     })
 };
--- models/genre.js
+++ models/genre.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const genre = new mongoose.Schema({
-    name: { type:String, min: 5, max: 20, required: true, trim: true }
+    name: { type: String, minlength: 5, maxlength: 20, required: true, trim: true }
 })
 const Genre = mongoose.model('Genre', genre);
 module.exports = Genre;
