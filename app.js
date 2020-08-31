f2682e61c22e7c1d9f6362cb018c50c14df57c99
dev-joe95 <joe.afify95@gmail.com>
2020-08-31 08:07:52 +0200

    adding production middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 // require("./startup/config")();
+require("./startup/prod")(app);
 
 app.use(express.json());
 app.use(logError);
